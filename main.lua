67067555932984f89f1ce99579658c84d651f2b3
Stefan <stevetv90@gmail.com>
2017-09-25 20:02:16 +0200

    Laserreflection code fixed
    Laserreflection for 1 color should now work

--- main.lua
+++ main.lua
@@ -31,14 +31,34 @@ function love.load()
             local x = (lasers[i].x+size)/size
             local y = (lasers[i].y+size)/size
             lasermap[x][y] = 2
-        end
-    end
-
-    for i = 1, #lasers do
-        if lasers[i].name == "Mirror1" then
+        elseif lasers[i].name == "Laser2" then
+            local x = (lasers[i].x+size)/size
+            local y = (lasers[i].y+size)/size
+            lasermap[x][y] = 12
+        elseif lasers[i].name == "Laser3" then
+            local x = (lasers[i].x+size)/size
+            local y = (lasers[i].y+size)/size
+            lasermap[x][y] = 22
+        elseif lasers[i].name == "Laser4" then
+            local x = (lasers[i].x+size)/size
+            local y = (lasers[i].y+size)/size
+            lasermap[x][y] = 32
+        elseif lasers[i].name == "Mirror1" then
             local x = (lasers[i].x+size)/size
             local y = (lasers[i].y+size)/size
             lasermap[x][y] = 3
+        elseif lasers[i].name == "Mirror2" then
+            local x = (lasers[i].x+size)/size
+            local y = (lasers[i].y+size)/size
+            lasermap[x][y] = 13
+        elseif lasers[i].name == "Mirror3" then
+            local x = (lasers[i].x+size)/size
+            local y = (lasers[i].y+size)/size
+            lasermap[x][y] = 23
+        elseif lasers[i].name == "Mirror4" then
+            local x = (lasers[i].x+size)/size
+            local y = (lasers[i].y+size)/size
+            lasermap[x][y] = 33
         end
     end
 end
@@ -52,20 +72,7 @@ function love.update(dt)
             end
         end
     end
-    for i = 1, mapsize.x do
-        for j = 1, mapsize.y do
-            if (lasermap[i][j] == 2 or lasermap[i][j] == 9) and i+1 <= mapsize.x then
-                if lasermap[i+1][j] ~= 1 and lasermap[i+1][j] ~= 2 and lasermap[i+1][j] ~= 3 then
-                    lasermap[i+1][j] = 9
-                end
-            end
-            if (lasermap[i][j] == 3 or lasermap[i][j] == 8) and j+1 <= mapsize.y then
-                if lasermap[i][j+1] ~= 1 and lasermap[i][j+1] ~= 2 and lasermap[i][j+1] ~= 3 then 
-                    lasermap[i][j+1] = 8
-                end
-            end
-        end
-    end
+    addLaserbeam()
 end
 
 function love.draw()
@@ -75,12 +82,95 @@ function love.draw()
     love.graphics.print(tostring(map.layers),200,200)
     for i = 1, mapsize.x do
         for j = 1, mapsize.y do
-            if lasermap[i][j] == 2 or lasermap[i][j] == 3 then
+            if lasermap[i][j] == 2 or lasermap[i][j] == 12 or lasermap[i][j] == 22 or lasermap[i][j] == 32 or lasermap[i][j] == 3 or lasermap[i][j] == 13 or lasermap[i][j] == 23 or lasermap[i][j] == 33 then
                 love.graphics.rectangle("fill", i*size-size, j*size-size, size, size)
-            elseif lasermap[i][j] == 8 or lasermap[i][j] == 9 then
+            elseif lasermap[i][j] == 9 then
                 love.graphics.rectangle("line", i*size-size, j*size-size, size, size)
             end
-            love.graphics.print(lasermap[i][j],300+i*10,200+j*10)
+            love.graphics.print(lasermap[i][j],300+i*20,100+j*20)
+        end
+    end
+end
+
+
+function addLaserbeam()
+    for i = 1, mapsize.x do
+        for j = 1, mapsize.y do
+            local movement = {x = 0, y = 0}
+            if lasermap[i][j] == 2 then
+                movement = {x = 1, y = 0}
+            elseif lasermap[i][j] == 12 then
+                movement = {x = 0, y = 1}
+            elseif lasermap[i][j] == 22 then
+                movement = {x = -1, y = 0}
+            elseif lasermap[i][j] == 32 then
+                movement = {x = 0, y = -1}
+            end
+            local hitwall = false
+            local current = {x = i, y = j}
+            local loop = 0
+            repeat
+                --Start
+                if (lasermap[current.x][current.y] == 2 or lasermap[current.x][current.y] == 12 or lasermap[current.x][current.y] == 22 or lasermap[current.x][current.y] == 32) and (lasermap[current.x+movement.x][current.y+movement.y] == 0 or lasermap[current.x+movement.x][current.y+movement.y] == 9) then
+                    lasermap[current.x+movement.x][current.y+movement.y] = 9
+                    current.x = current.x+movement.x
+                    current.y = current.y+movement.y
+                --Follow
+                elseif lasermap[current.x][current.y] == 9 and (lasermap[current.x+movement.x][current.y+movement.y] == 0 or lasermap[current.x+movement.x][current.y+movement.y] == 9) then
+                    lasermap[current.x+movement.x][current.y+movement.y] = 9
+                    current.x = current.x+movement.x
+                    current.y = current.y+movement.y
+                --Left Down
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 3 and movement.x == 1 and movement.y == 0 and (lasermap[current.x+movement.x][current.y+movement.y+1] == 0 or lasermap[current.x+movement.x][current.y+movement.y+1] == 9) then
+                    lasermap[current.x+movement.x][current.y+movement.y+1] = 9
+                    current.x = current.x+movement.x
+                    current.y = current.y+movement.y+1
+                    movement = {x = 0, y = 1}
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 3 and movement.x == 0 and movement.y == -1 and (lasermap[current.x+movement.x-1][current.y+movement.y] == 0 or lasermap[current.x+movement.x-1][current.y+movement.y] == 9) then
+                    lasermap[current.x+movement.x-1][current.y+movement.y] = 9
+                    current.x = current.x+movement.x-1
+                    current.y = current.y+movement.y
+                    movement = {x = -1, y = 0}
+                --Top Left
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 13 and movement.x == 0 and movement.y == 1 and (lasermap[current.x+movement.x-1][current.y+movement.y] == 0 or lasermap[current.x+movement.x-1][current.y+movement.y] == 9) then
+                    lasermap[current.x+movement.x-1][current.y+movement.y] = 9
+                    current.x = current.x+movement.x-1
+                    current.y = current.y+movement.y
+                    movement = {x = -1, y = 0}
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 13 and movement.x == 1 and movement.y == 0 and (lasermap[current.x+movement.x][current.y+movement.y-1] == 0 or lasermap[current.x+movement.x][current.y+movement.y-1] == 9) then
+                    lasermap[current.x+movement.x][current.y+movement.y-1] = 9
+                    current.x = current.x+movement.x
+                    current.y = current.y+movement.y-1
+                    movement = {x = 0, y = -1}
+                --Right Top
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 23 and movement.x == -1 and movement.y == 0 and (lasermap[current.x+movement.x][current.y+movement.y-1] == 0 or lasermap[current.x+movement.x][current.y+movement.y-1] == 9) then
+                    lasermap[current.x+movement.x][current.y+movement.y-1] = 9
+                    current.x = current.x+movement.x
+                    current.y = current.y+movement.y-1
+                    movement = {x = 0, y = -1}
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 23 and movement.x == 0 and movement.y == -1 and (lasermap[current.x+movement.x+1][current.y+movement.y] == 0 or lasermap[current.x+movement.x+1][current.y+movement.y] == 9) then
+                    lasermap[current.x+movement.x+1][current.y+movement.y] = 9
+                    current.x = current.x+movement.x+1
+                    current.y = current.y+movement.y
+                    movement = {x = 1, y = 0}
+                --Down Right
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 33 and movement.x == 0 and movement.y == -1 and (lasermap[current.x+movement.x+1][current.y+movement.y] == 0 or lasermap[current.x+movement.x+1][current.y+movement.y] == 9) then
+                    lasermap[current.x+movement.x+1][current.y+movement.y] = 9
+                    current.x = current.x+movement.x+1
+                    current.y = current.y+movement.y
+                    movement = {x = 1, y = 0}
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 33 and movement.x == -1 and movement.y == 0 and (lasermap[current.x+movement.x][current.y+movement.y+1] == 0 or lasermap[current.x+movement.x][current.y+movement.y+1] == 9) then
+                    lasermap[current.x+movement.x][current.y+movement.y+1] = 9
+                    current.x = current.x+movement.x
+                    current.y = current.y+movement.y+1
+                    movement = {x = 0, y = 1}
+                elseif lasermap[current.x+movement.x][current.y+movement.y] == 1 then
+                    hitwall = true
+                else
+                    hitwall = true
+                end
+                loop = loop + 1
+            until hitwall == true or loop >= mapsize.x*mapsize.y
         end
     end
 end
